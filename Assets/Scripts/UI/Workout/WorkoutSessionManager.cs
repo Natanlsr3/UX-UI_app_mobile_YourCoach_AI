ff94e33cb827b14d385145abcd7a0a8661f2a3da
MJSAMA <acerisey4@gmail.com>
2026-04-10 18:09:08 +0200

    fix(workout) fix switch to alternate exercise

--- Assets/Scripts/UI/Workout/WorkoutSessionManager.cs
+++ Assets/Scripts/UI/Workout/WorkoutSessionManager.cs
@@ -386,7 +386,7 @@ namespace MVC.App.UI.Workout
                 SoundManager.instance.PlayClip();
             }
             yield return new WaitWhile(()=>SoundManager.instance.soundSource.isPlaying);
-            if (exerciseNumber >= currentExercise.SetNumber)
+            if (exerciseIndex >= LogSession.Instance.WorkoutExercises.Count-1)
             {
                 //!!!!!Warning works only in this demo would have to rework the system to include on the fly modification later
                 LogSession.Instance.WorkoutExercises[exerciseIndex] = LogSession.Instance.OptExercises[0];
