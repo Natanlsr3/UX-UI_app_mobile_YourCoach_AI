e03a9800bb160952b84e026c2b55eb580f43007e
Quentin <qmartherose@gmail.com>
2026-04-30 16:36:42 +0200

    fix(login) : fixed login and account creation to work with new session system

--- Assets/Scripts/UI/RegisterMenu/RegisterPopUp/ForgetPassword.cs
+++ Assets/Scripts/UI/RegisterMenu/RegisterPopUp/ForgetPassword.cs
@@ -13,9 +13,13 @@ namespace MVC.App.UI.RegisterMenu.RegisterPopUp
 
         private void ResetPassword()
         {
-            if (m_TypedUsername == m_Username)
+            if (LogSession.Instance.ExistingUser(m_TypedUsername) && m_TypedPassword != "")
             {
                 OnPasswordReset?.Invoke(m_TypedPassword);
+
+                LogSession.User _user = LogSession.Instance.FindUser(m_TypedUsername);
+                _user.Password = m_TypedPassword;
+                LogSession.Instance.UserList[LogSession.Instance.FindUserIndex(m_TypedUsername)] = _user;
                 LeavePopUp();
             }
             else DisplayError(true);
